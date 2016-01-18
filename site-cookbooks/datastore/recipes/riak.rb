f7a17b4430370128a988f38d390b55ee480800b0
Hidekazu Tanaka <hidekazu.tanaka@gmail.com>
2016-01-18 23:34:53 +0900

    berks update

--- site-cookbooks/datastore/recipes/riak.rb
+++ site-cookbooks/datastore/recipes/riak.rb
@@ -1,15 +1,4 @@
-node.default['sysctl']['params']['vm']['swappiness'] = node['riak']['sysctl']['vm']['swappiness']
-node.default['sysctl']['params']['net']['core']['somaxconn'] = node['riak']['sysctl']['net']['core']['somaxconn']
-node.default['sysctl']['params']['net']['ipv4']['tcp_max_syn_backlog'] = node['riak']['sysctl']['net']['ipv4']['tcp_max_syn_backlog']
-node.default['sysctl']['params']['net']['ipv4']['tcp_sack'] = node['riak']['sysctl']['net']['ipv4']['tcp_sack']
-node.default['sysctl']['params']['net']['ipv4']['tcp_window_scaling'] = node['riak']['sysctl']['net']['ipv4']['tcp_window_scaling']
-node.default['sysctl']['params']['net']['ipv4']['tcp_fin_timeout'] = node['riak']['sysctl']['net']['ipv4']['tcp_fin_timeout']
-node.default['sysctl']['params']['net']['ipv4']['tcp_keepalive_intvl'] = node['riak']['sysctl']['net']['ipv4']['tcp_keepalive_intvl']
-node.default['sysctl']['params']['net']['ipv4']['tcp_tw_reuse'] = node['riak']['sysctl']['net']['ipv4']['tcp_tw_reuse']
-node.default['sysctl']['params']['net']['ipv4']['tcp_moderate_rcvbuf'] = node['riak']['sysctl']['net']['ipv4']['tcp_moderate_rcvbuf']
-
 include_recipe 'ulimit'
-include_recipe 'sysctl'
 
 user_ulimit 'riak' do
   filehandle_limit node['riak']['limits']['nofile']
@@ -18,14 +7,14 @@ end
 include_recipe 'riak::package'
 
 file "#{node['riak']['platform_etc_dir']}/riak.conf" do
-  content Cuttlefish.compile('', node['riak']['config']).join("\n")
+  content lazy { Cuttlefish.compile('', node['riak']['config']).join("\n") }
   owner 'root'
   mode 0644
   notifies :restart, 'service[riak]'
 end
 
 node['riak']['patches'].each do |patch|
-  cookbook_file "#{node['riak']['platform_data_dir']}/lib/basho-patches/#{patch}" do
+  cookbook_file "#{node['riak']['platform_lib_dir']}/basho-patches/#{patch}" do
     source patch
     owner 'root'
     mode 0644
